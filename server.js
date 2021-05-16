ce52e59e1fc6b1bcf53e105fb046d353529ae0f9
quangtran12345 <ooosurikenooo@gmail.com>
2021-05-16 21:09:01 +0700

    edit file webpack

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var path = require('path');
 var express = require('express');
 
-var app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+var app = express();
+app.use(express.static(path.join(__dirname, '/')))
+
+// app.use('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, "dist", "index.html"));
+// })
 app.set('port', process.env.PORT || 4000);
 
 var server = app.listen(app.get('port'), function() {
